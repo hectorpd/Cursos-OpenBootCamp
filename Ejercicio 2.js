2175d1a6a41b45eba2bcdfaebb5f1c9ef3d363b3
Hector Padron <110635106+hectorpd@users.noreply.github.com>
2023-01-03 22:14:33 +0100

    Add files via upload

---
+++ Ejercicio 2.js
@@ -0,0 +1,12 @@
+//Ejercicio 2
+const Lista= new Array(5)
+Lista[0]= "Hector Padron";
+Lista[1]= 29;
+Lista[2]= desarrollador= true;
+Lista[3]= fecha_nacimiento = new Date(1993, 1, 24);
+Lista[4]=  libro = {
+       titulo: "Ensayo sobre la ceguera",
+       autor: "Josè Saramago",
+       fecha: 1995,
+       url: new URL("https://books.google.es/books/about/Ensayo_sobre_la_ceguera.html?id=tKjAj5rDY0sC&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"),
+}
